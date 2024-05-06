10a7a2f4fdebcef864abb3f98754674c9854e287
youngboypimpin <rjtekdom@gmail.com>
2024-05-06 14:14:15 +0100

    properly set up the site with configurations next is to work on the featured posts

---
+++ application/views/frontend/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer mt-auto py-3 bg-dark">
+    <div class="container text-center">
+        <span class="text-muted"> contact us at  <a href="bigtechdomain@gmail.com">bigtechdomain@gmail.com</a>
+        <p> &copy; All Rights Reserved| by bigTechdomain</p></span>
+    </div>
+</footer>
+     <script src="main.js"></script>
+    <script src="assets/dist/js/bootstrap.bundle.min.js"></script>  
+</body>
+</html>
---
+++ application/views/frontend/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Movies Bay</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="assets/images/favicon.ico?">
+    <link rel="stylesheet" href="assets/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/stylesheets/IDX-3.css"> 
+</head>  
+<body>
+    <header class="p-3 bg-dark text-white">
+        <div class="container">
+        <span class="logo">Movies Bay</span>
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <ul class="nav col-12 col-lg-4 me-lg-4 mb-2 justify-content-center mb-md-0">
+                <li><a href="#" class="nxt" class="nav-link px-2 text-secondary">Home</a></li>
+                <li><a href="#" class="nxt" class="nav-link px-2 text-white">New Releases</a></li>
+                <li><a href="#" class="nxt" class="nav-link px-2 text-white">Trending</a></li>
+                <li><a href="#" class="nxt" class="nav-link px-2 text-white">Movies</a></li>
+                <li><a href="#" class="nxt" class="nav-link px-2 text-white">Series</a></li>
+            </ul>      
+            <form class=" nav-form col-12 col-lg-4 mb-3 mb-lg-0 me-lg-3">
+                <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+            </form>     
+            <div class="text-end">
+                <button type="button" class="btn btn-outline-light me-2">Login</button>
+                <button type="button" class="btn btn-warning">Sign-up</button>
+            </div>
+        </div>
+        </div>
+    </header>
