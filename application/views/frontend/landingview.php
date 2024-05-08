08a67ef896f7d02a07711899efda883d88bf5aba
youngboypimpin <rjtekdom@gmail.com>
2024-05-08 15:51:33 +0100

    n

--- application/views/frontend/landingview.php
+++ application/views/frontend/landingview.php
@@ -11,27 +11,27 @@
         <section class=" col-lg-10 col-md-8 col-sm-4" >
             <h5>New Releases</h5>
             <div class="container-fluid movie-section ">
-                <a class="movie-link" href="Videos/test movie.mp4">
+                <a class="movie-link" href="assets/video/movie.mp4">
                    <img class="movie-thumbnail" src="assets/images/MOVIE POSTERS/jjk poster.jpeg"
                     alt="movie thumbnail" width="130px" height="200px"> 
                 </a>
-                <a class="movie-link" href="Videos/test movie.mp4">
+                <a class="movie-link" href="assets/video/movie.mp4">
                    <img class="movie-thumbnail" src="assets/images/MOVIE POSTERS/Morbius poster.jpeg"
                     alt="movie thumbnail" width="130px" height="200px"> 
                 </a>
-                <a class="movie-link" href="Videos/test movie.mp4">
+                <a class="movie-link" href="assets/video/movie.mp4">
                     <img class="movie-thumbnail" src="assets/images/MOVIE POSTERS/Multiverse of Madness Poster.jpeg" 
                     alt="movie thumbnail" width="130px" height="200px"> 
                 </a>
-                <a class="movie-link" href="Videos/test movie.mp4">
+                <a class="movie-link" href="assets/video/movie.mp4">
                     <img class="movie-thumbnail" src="assets/images/MOVIE POSTERS/prey poster.jpeg" 
                     alt="movie thumbnail" width="130px" height="200px"> 
                 </a>
-                <a class="movie-link" href="Videos/test movie.mp4">
+                <a class="movie-link" href="assets/video/movie.mp4">
                     <img class="movie-thumbnail" src="assets/images/MOVIE POSTERS/IMG_20221103_225742_501.jpg" 
                     alt="movie thumbnail" width="130px" height="200px" >
                 </a>
-                <a class="movie-link" href="Videos/test movie.mp4">
+                <a class="movie-link" href="assets/video/movie.mp4">
                     <img class="movie-thumbnail" src="assets/images/MOVIE POSTERS/IMG_20221103_230038_906.jpg" 
                     alt="movie thumbnail" width="130px" height="200px" >
                 </a>
@@ -43,27 +43,27 @@
         <section class=" col-lg-10 col-md-8 col-sm-4">
             <h5>Trending </h5>
             <div class="container-fluid movie-section ">
-                <a class="movie-link" href="Videos/test movie.mp4">
+                <a class="movie-link" href="assets/video/movie.mp4">
                    <img class="movie-thumbnail" src="assets/images/MOVIE POSTERS/4603354.jpg" 
                    alt="movie thumbnail" width="130px" height="200px"> 
                 </a>
-                <a class="movie-link" href="Videos/test movie.mp4">
+                <a class="movie-link" href="assets/video/movie.mp4">
                    <img class="movie-thumbnail" src="assets/images/MOVIE POSTERS/images.jpeg" 
                    alt="movie thumbnail" width="130px" height="200px"> 
                 </a>
-                <a class="movie-link" href="Videos/test movie.mp4">
+                <a class="movie-link" href="assets/video/movie.mp4">
                     <img class="movie-thumbnail" src="assets/images/MOVIE POSTERS/Spiderman No Way Home Poster.jpeg" 
                     alt="movie thumbnail" width="130px" height="200px"> 
                 </a>
-                <a class="movie-link" href="Videos/test movie.mp4">
+                <a class="movie-link" href="assets/video/movie.mp4">
                     <img class="movie-thumbnail" src="assets/images/MOVIE POSTERS/The Adam Project poster.jpeg" 
                     alt="movie thumbnail" width="130px" height="200px"> 
                 </a>
-                <a class="movie-link" href="Videos/test movie.mp4">
+                <a class="movie-link" href="assets/video/movie.mp4">
                     <img class="movie-thumbnail" src="assets/images/MOVIE POSTERS/Uncharted poster.jpeg" 
                     alt="movie thumbnail" width="130px" height="200px" >
                 </a>
-                <a class="movie-link" href="Videos/test movie.mp4">
+                <a class="movie-link" href="assets/video/movie.mp4">
                     <img class="movie-thumbnail" src="assets/images/MOVIE POSTERS/IMG_20221103_112529_904.jpg" 
                     alt="movie thumbnail" width="130px" height="200px" >
                 </a>
@@ -75,27 +75,27 @@
         <section class=" col-lg-10 col-md-8 col-sm-4">
             <h5>Highest Ratings</h5>
             <div class="container-fluid movie-section ">
-                <a class="movie-link" href="Videos/test movie.mp4">
+                <a class="movie-link" href="assets/video/movie.mp4">
                    <img class="movie-thumbnail" src="assets/images/MOVIE POSTERS/the batman poster.jpeg" 
                    alt="movie thumbnail" width="130px" height="200px"> 
                 </a>
-                <a class="movie-link" href="Videos/test movie.mp4">
+                <a class="movie-link" href="assets/video/movie.mp4">
                    <img class="movie-thumbnail" src="assets/images/MOVIE POSTERS/The Northman.jpeg" 
                    alt="movie thumbnail" width="130px" height="200px"> 
                 </a>
-                <a class="movie-link" href="Videos/test movie.mp4">
+                <a class="movie-link" href="assets/video/movie.mp4">
                     <img class="movie-thumbnail" src="assets/images/MOVIE POSTERS/The Princess.jpeg" 
                     alt="movie thumbnail" width="130px" height="200px"> 
                 </a>
-                <a class="movie-link" href="Videos/test movie.mp4">
+                <a class="movie-link" href="assets/video/movie.mp4">
                     <img class="movie-thumbnail" src="assets/images/MOVIE POSTERS/The-Man-From-Toronto-2022-movie-poster.jpg" 
                     alt="movie thumbnail"width="130px" height="200px"> 
                 </a>
-                <a class="movie-link" href="Videos/test movie.mp4">
+                <a class="movie-link" href="assets/video/movie.mp4">
                     <img class="movie-thumbnail" src="assets/images/MOVIE POSTERS/sex-education movie poster.jpg" 
                     alt="movie thumbnail" width="130px" height="200px">
                 </a>
-                <a class="movie-link" href="Videos/test movie.mp4">
+                <a class="movie-link" href="assets/video/movie.mp4">
                     <img class="movie-thumbnail" src="assets/images/MOVIE POSTERS/new amsterdam movie poster.jpg" 
                     alt="movie thumbnail" width="130px" height="200px" >
                 </a>
